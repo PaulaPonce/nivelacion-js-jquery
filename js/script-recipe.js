33ea186f4b96e3af2d9d6bdc071331904685d632
Paula Ponce <paula.poncez@usach.cl>
2017-07-11 00:29:22 -0400

    Etapa 8: se añade funcionalidad a pestañas, evento click en pestaña js-show-make para agregar clase make en div y en pestaña js-show-recipe para quitarle la clase make

--- js/script-recipe.js
+++ js/script-recipe.js
@@ -2,4 +2,22 @@ $(document).ready(function(){
 	console.log("Documento listo");
 	//Oculta el menú cuando se muestra recipe.html
 	$(".js-menu").hide();
-})
+
+	/*
+	* Función que quita la clase make a div
+	*/
+	$(".js-show-recipe").on("click", removeMakeClass);
+	function removeMakeClass(){
+		$("div.recipe").removeClass("make");
+	}
+
+	/*
+	* Función que agrega la clase make a div
+	*/
+	$(".js-show-make").on("click", addMakeClass);
+	function addMakeClass(){
+		$("div.recipe").addClass("make");
+	}
+});
+
+	
