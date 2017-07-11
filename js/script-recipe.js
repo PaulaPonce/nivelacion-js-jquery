f440528b9b820482a7e7c37572bbf6f061c12865
Paula Ponce <paula.poncez@usach.cl>
2017-07-11 00:42:23 -0400

    Etapa 9: evento click en pestañas js-show-recipe y js-show-make para agregar y quitar la clase active, según que en qué pestaña se haga click

--- js/script-recipe.js
+++ js/script-recipe.js
@@ -5,19 +5,23 @@ $(document).ready(function(){
 
 	/*
 	* Función que quita la clase make a div
+	* Añade clase active a pestaña js-show-recipe y se la quita a pestaña js-show-make
 	*/
-	$(".js-show-recipe").on("click", removeMakeClass);
-	function removeMakeClass(){
+	$(".js-show-recipe").on("click", function(){
+		$(this).addClass("active");
+		$(".js-show-make").removeClass("active");
 		$("div.recipe").removeClass("make");
-	}
+	});
 
 	/*
 	* Función que agrega la clase make a div
+	* Añade clase active a pestaña js-show-make y se la quita a pestaña js-show-recipe
 	*/
-	$(".js-show-make").on("click", addMakeClass);
-	function addMakeClass(){
+	$(".js-show-make").on("click", function(){
+		$(this).addClass("active");
+		$(".js-show-recipe").removeClass("active");
 		$("div.recipe").addClass("make");
-	}
+	});
 });
 
 	
